6b31103cd269a6af5773b787ed2c60304b1baee1
Peedrooo <pedrovitora.jesus@gmail.com>
2022-03-26 11:31:53 -0300

    Removendo codigo

--- website-coleta/src/components/common/wrapper.style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrapper = styled.div`
-    width: 100%;
-    height: 100%;
-    background-image: url('../../public/bg-theme.png');
-    z-index: 10 ;
-`;
